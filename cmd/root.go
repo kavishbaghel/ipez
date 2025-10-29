50bf7b5ddb5f87406da5b6cb60c117e86ababc28
Kavish Baghel <kavishbaghel@gmail.com>
2025-10-30 05:09:05 +0530

    Cleaned up folder for building from scratch and added gitignore

--- cmd/root.go
+++ cmd/root.go
@@ -1,7 +1,41 @@
-package main
+/*
+Copyright © 2025 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
 
-import "fmt"
+import (
+	"os"
 
-func newCmd() {
-	fmt.Print("Command runs here...")
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "iptool",
+	Short: "A lightweight cli tool for engineers to quickly perform IP address and Subnet related tasks.",
+	Long:  `iptool is a lightweight command-line utility built in Go for developers, DevOps engineers, and network admins who work with IP ranges, subnets, and CIDRs on a daily basis. It provides quick, practical operations like viewing subnet info, converting CIDRs to ranges, splitting or merging networks, and validating IPs, etc..`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.iptool.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
